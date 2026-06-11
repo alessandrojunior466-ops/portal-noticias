bbefd34b179168f86a24a1e892b4ebff8885bb85
Alessandro Junior <alessandrojunior466@gmail.com>
2026-06-11 14:42:51 -0300

    criação de tabela categoria

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -43,7 +43,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('usuario');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
